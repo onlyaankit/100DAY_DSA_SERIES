b081d9634cab9d5943010b5d9a40203be2b2fc15
onlyaankit <workforaankit1@gmail.com>
2024-12-05 13:55:27 +0530

    maxOccuringElement in String

--- Day01/maxOccElement.cpp
+++ Day01/maxOccElement.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+char maxOccuringElement(string str){
+    unordered_map<char, int> freq;
+    int n = str.length();
+    char ans;
+    int count = 0;
+    for(int i=0; i<n; i++){
+        freq[str[i]]++;
+        if(count < freq[str[i]]){
+            ans = str[i];
+            count = freq[str[i]];
+        }
+    }
+    return ans; // final answer 
 }
 
+int main(){
+    string str = "geeksforgeeks";
+    cout<< "Max occourence of Element: " << maxOccuringElement(str) << endl;
+}
